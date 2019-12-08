35b2d61654eadb828e3b424189006d0549446111
RednedEpic <redned235@gmail.com>
2019-12-08 12:08:23 -0600

    Implement option to modify friendly fire

--- src/java/mc/alk/paintball/Paintball.java
+++ src/java/mc/alk/paintball/Paintball.java
@@ -5,10 +5,11 @@ import mc.alk.arena.util.Log;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-public class Paintball extends JavaPlugin{
+
+public class Paintball extends JavaPlugin {
 
 	@Override
-	public void onEnable(){
+	public void onEnable() {
 		/// Registers this plugin with BattleArena
 		/// this: our plugin
 		/// "Paintball": The name of our competition
@@ -27,21 +28,21 @@ public class Paintball extends JavaPlugin{
 	}
 
 	@Override
-	public void onDisable(){
+	public void onDisable() {
 		Log.info("[" + getName()+ "] v" + getDescription().getVersion()+ " stopping!");
 	}
 
 	@Override
-	public void reloadConfig(){
+	public void reloadConfig() {
 		super.reloadConfig();
 		loadConfig();
 	}
 
-	public void loadConfig(){
+	public void loadConfig() {
 		/// Allow the damage to be set through the config.yml, if it exists and has the section: 'damage: <value>'
 		/// Like 'damage: 15'
 		FileConfiguration config = getConfig();
 		PaintballArena.damage = config.getInt("damage", 20);
+		PaintballArena.friendlyFire = config.getBoolean("friendlyFire", true);
 	}
-
 }
--- src/java/mc/alk/paintball/PaintballArena.java
+++ src/java/mc/alk/paintball/PaintballArena.java
@@ -4,14 +4,20 @@ import mc.alk.arena.objects.arenas.Arena;
 import mc.alk.arena.objects.events.ArenaEventHandler;
 
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.projectiles.ProjectileSource;
 
-public class PaintballArena extends Arena{
+import java.util.Objects;
+
+public class PaintballArena extends Arena {
+
+	static boolean friendlyFire;
 	static int damage = 20;
 
 	/**
-	 * This is how you create customized events.  You specify a method as a @MatchEventHandler
+	 * This is how you create customized events.  You specify a method as a {@link ArenaEventHandler}
 	 * and give it at least one bukkit event as the first argument.  In this case its EntityDamageEvent
 	 * These events will ONLY be called when a match is ongoing
 	 * If the event returns a player (in this case it does) then the event only gets called when
@@ -20,11 +26,22 @@ public class PaintballArena extends Arena{
 	 *
 	 * @param event: Which bukkit event are we listening to
 	 */
-	@ArenaEventHandler(suppressCastWarnings=true)
-	public void onEntityDamage(EntityDamageEvent event) {
-		if (event.isCancelled() || !(event instanceof EntityDamageByEntityEvent) ||
-				((EntityDamageByEntityEvent)event).getDamager().getType() != EntityType.SNOWBALL)
+	@ArenaEventHandler(suppressCastWarnings = true)
+	public void onEntityDamage(EntityDamageByEntityEvent event) {
+		if (!(event.getEntity() instanceof Player) || event.getDamager().getType() != EntityType.SNOWBALL)
 			return;
+
+		ProjectileSource source = ((Projectile) event.getDamager()).getShooter();
+		if (!(source instanceof Player))
+			return;
+
+		Player hit = (Player) event.getEntity();
+		Player shooter = (Player) source;
+
+		if (!friendlyFire)
+			if (Objects.equals(getTeam(hit), getTeam(shooter)))
+				return;
+
 		event.setDamage(damage);
 	}
 }
